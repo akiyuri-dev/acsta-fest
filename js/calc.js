bbfd426d350619edbb0ad8b3024b8baf865f65db
Haruna Karasawa <harunakarasawa@Harunas-MacBook-Pro.local>
2022-09-09 18:07:47 +0900

    price

--- js/calc.js
+++ js/calc.js
@@ -6,7 +6,7 @@ $('input[type="number"]').bind('input', function () {
         Allnum += Number(stars[i].value);
     }
     
-    var All = Allnum*1000;
+    var All = Allnum*1200;
 
     document.getElementById("result-erea").innerHTML = "<h3 class='text-danger'>合計" + Allnum + "点</h3>" + "<h3 class='text-danger'>" + All + "円</h3>";
 });
